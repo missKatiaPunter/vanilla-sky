95af9d69415dea1bffec895aaac2da7d5b153cad
Katia <katia.punter@storyshareplatform.com>
2021-09-20 08:29:07 +0100

    bits

--- consolemeth.js
+++ consolemeth.js
@@ -1,4 +1,27 @@
+console.log(123);
+
+console.table({a:1,b:2});
+
 let myDoc;
 
 console.log("This is myDoc", myDoc);
 
+myDoc = document.body;
+
+console.log("This is myDoc", myDoc);
+
+myDoc = document.URL;
+
+console.log("This is myDoc", myDoc);
+
+myDoc = document;
+
+console.log("This is myDoc", myDoc);
+
+myDoc = document.all.length;
+
+console.log("This is myDoc", myDoc);
+
+myDoc = document.images;
+
+console.log("This is myDoc", myDoc);
